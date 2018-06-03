238da68fcb2a27c0fd15264d7666a89afe7a79d8
Nuno Santos <nunojsantos@hotmail.com>
2018-06-03 16:01:02 +0100

    feature: character; starting to test

--- src/org/academiadecodigo/bootcamp/wormgame/Character.java
+++ src/org/academiadecodigo/bootcamp/wormgame/Character.java
@@ -1,16 +1,23 @@
 package org.academiadecodigo.bootcamp.wormgame;
 
+import org.academiadecodigo.bootcamp.physics2D.Body2D.CircularBody2D;
+import org.academiadecodigo.bootcamp.physics2D.utils.Vector2D;
+
 /**
  * Created by codecadet on 03/06/2018.
  */
-public class Character implements Hittable {
+public class Character extends CircularBody2D implements Hittable {
 
     //private Player player;
     private int health;
     //private aim;
     //private WeaponType weaponType;
 
-    
+    public Character(Vector2D position) {
+        super(0,2, position);
+    }
+
+
     public void move() {
 
     }
---
+++ src/org/academiadecodigo/bootcamp/wormgame/TestCharacter.java
@@ -0,0 +1,15 @@
+package org.academiadecodigo.bootcamp.wormgame;
+
+/**
+ * Created by codecadet on 03/06/2018.
+ */
+public class TestCharacter {
+
+    public static void main(String[] args) {
+
+        Character character = new Character()
+
+    }
+
+
+}
