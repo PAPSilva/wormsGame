c309d34504478caace3ed48248b2be304d0b16e9
Nuno Santos <nunojsantos@hotmail.com>
2018-06-03 18:34:47 +0100

    feature: character. testing and minor changes

--- src/org/academiadecodigo/bootcamp/wormgame/Character.java
+++ src/org/academiadecodigo/bootcamp/wormgame/Character.java
@@ -3,6 +3,8 @@ package org.academiadecodigo.bootcamp.wormgame;
 import org.academiadecodigo.bootcamp.physics2D.Body2D.CircularBody2D;
 import org.academiadecodigo.bootcamp.physics2D.utils.Vector2D;
 
+import java.util.LinkedList;
+
 /**
  * Created by codecadet on 03/06/2018.
  */
@@ -10,24 +12,42 @@ public class Character extends CircularBody2D implements Hittable {
 
     //private Player player;
     private int health;
-    //private aim;
-    //private WeaponType weaponType;
+    private double aim;
+    //private Weapon currentWeapon;
 
     public Character(Vector2D position) {
-        super(0,2, position);
-    }
 
-
-    public void move() {
+        super(0,30, position);
+        this.health = 100;
 
     }
 
+
+    public void move(double x, double y) {
+
+        this.getPosition().add(x, y);
+
+    }
+
+
+    // for now, receives a double that can be positive or negative. it can increase or decrease aim.
+    public void changeAim(double a) {
+
+        this.aim += a;
+
+    }
+
+
+    // the fire method needs the weapon type
     public void fire() {
 
     }
 
+
     public void suffer(int sufferDamage){
+
         health -= sufferDamage;
+
     }
 
 
