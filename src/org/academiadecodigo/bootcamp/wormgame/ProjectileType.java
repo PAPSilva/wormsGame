60230a1e72614df312774f91508ac0edbd85dcd4
Andre Carreira <andre.carreira@gmail.com>
2018-06-04 21:15:09 +0100

    Created Player class

--- src/org/academiadecodigo/bootcamp/wormgame/ProjectileType.java
+++ src/org/academiadecodigo/bootcamp/wormgame/ProjectileType.java
@@ -4,8 +4,8 @@ package org.academiadecodigo.bootcamp.wormgame;
  * Created by codecadet on 03/06/2018.
  */
 public enum ProjectileType {
-    BULLET(40,10,10),
-    ROCKET(80,800,40);
+    BULLET(40,5,10),
+    ROCKET(80,10,40);
 
 
 
