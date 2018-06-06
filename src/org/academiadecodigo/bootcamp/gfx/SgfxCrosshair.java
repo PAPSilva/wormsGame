c6bbf9727675a5bdc3007ca71808e99172c75b85
Nuno Santos <nunojsantos@hotmail.com>
2018-06-06 23:30:09 +0100

    nothing added

--- src/org/academiadecodigo/bootcamp/gfx/SgfxCrosshair.java
+++ src/org/academiadecodigo/bootcamp/gfx/SgfxCrosshair.java
@@ -1,16 +1,28 @@
 package org.academiadecodigo.bootcamp.gfx;
 
+import javafx.scene.shape.Ellipse;
+import org.academiadecodigo.bootcamp.physics2D.Body2D.CircularBody2D;
 import org.academiadecodigo.bootcamp.physics2D.utils.Vector2D;
 
 /**
  * Created by codecadet on 06/06/2018.
  */
-public class SgfxCrosshair {
+public class SgfxCrosshair extends CircularBody2D {
 
     private Vector2D position;
+    private Ellipse crosshair;
 
-    public SgfxCrosshair(Vector2D position) {
-        this.position = new Vector2D(position);
+    public SgfxCrosshair(double aim, Vector2D playerPosition) {
+
+        super(0, 1, playerPosition);
+        this.position = new Vector2D(playerPosition);
+
+    }
+
+
+
+    public void changePosition(double aim, Vector2D playerPosition) {
+
     }
 
 }
