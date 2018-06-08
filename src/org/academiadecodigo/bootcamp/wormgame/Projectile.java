8a1b80c853143969f0f5efae0760756a49d9ffad
Nuno Santos <nunojsantos@hotmail.com>
2018-06-08 11:29:32 +0100

    feature: projectile, now is hittable

--- src/org/academiadecodigo/bootcamp/wormgame/Projectile.java
+++ src/org/academiadecodigo/bootcamp/wormgame/Projectile.java
@@ -6,22 +6,28 @@ import org.academiadecodigo.bootcamp.physics2D.utils.Vector2D;
 /**
  * Created by codecadet on 03/06/2018.
  */
-public class Projectile extends CircularBody2D {
+public class Projectile extends CircularBody2D implements Hittable {
 
 
     private ProjectileType projectileType;
+    private int active;
 
 
     public Projectile(ProjectileType projectileType, Vector2D position){
 
         super(projectileType.getMass(), projectileType.getRadius(), position);
         this.projectileType = projectileType;
+        this.active = 1;
     }
 
     public void hit(Hittable hittable) {
 
         hittable.suffer(projectileType.getAmmoDamage());
 
+    }
+
+    public void suffer(int damage) {
+        this.active -= damage;
     }
 
     public ProjectileType getProjectileType() {
